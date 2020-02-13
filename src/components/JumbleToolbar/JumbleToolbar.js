35e40683bd9e2cd3ab805f1c5672d3207c935dd0
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-13 11:11:51 +1100

    Made Jumble Toolbar component for Jumble Page

---
+++ src/components/JumbleToolbar/JumbleToolbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './JumbleToolbar.css';
+
+const JumbleToolbar = ({ svgs, handleSetJumble, handleReset }) => {
+
+  const jumbleArr = () => {
+    const newArr = new Array(26).fill(null)
+    const randomPos = () => Math.floor(Math.random() * 26);
+    svgs.forEach(el => {
+      let found = false;
+      let random = randomPos();
+      while (!found) {
+        if (!newArr[random]) {
+          newArr[random] = el;
+          found = true;
+        } else {
+          random = randomPos();
+        };
+      };
+    });
+    handleSetJumble(newArr);
+  };
+
+  return (
+    <header className="toolbar">
+      <nav className="toolbar__navigation">
+        <div>
+          <DrawerToggleButton />
+        </div>
+        <div className="toolbar__logo">
+          <a href="/">letter jumble</a>
+        </div>
+        <div className="spacer" />
+        <div className="toolbar_navigation-items">
+          <ul>
+            <li>
+              <button onClick={jumbleArr} id="jumble">Jumble</button>
+            </li>
+            <li>
+              <button onClick={handleReset} id="reset">Reset</button>
+            </li>
+            <li>
+              <a href="/spelling">
+                <button id="spelling">Spelling</button>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default JumbleToolbar;
