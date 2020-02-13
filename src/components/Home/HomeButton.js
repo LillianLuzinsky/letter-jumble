66a1a65c62f1d2f8d36b3a26941a525786d98915
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-14 09:33:51 +1100

    Finsihed homepage styling

--- src/components/Home/HomeButton.js
+++ src/components/Home/HomeButton.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const HomeButtonStyling = styled.button`
   color: white;
   text-decoration: none;
-  background: rgba(0, 0, 250, 0.3);
-  background:url("https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-double-twelve-purple-gradient-e-commerce-promotion-geometry-banner-image_183414.jpg") no-repeat center;
-  background-size: cover;
+  background: rgba(0, 0, 0, 0.3);
+  /* background:url("https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-double-twelve-purple-gradient-e-commerce-promotion-geometry-banner-image_183414.jpg") no-repeat center;
+  background-size: cover; */
   border: 2px solid white;
   border-radius: 30px;
   font-size: 2rem;
@@ -19,28 +19,31 @@ const HomeButtonStyling = styled.button`
   margin-bottom: 30px;
   transition: 0s;
 
-  :hover{
-    background: purple;
-    border: 2px solid purple;
+  :hover {
+    background: darkcyan;
+    border: 2px solid aquamarine;
     transition: 1s;
   }
-
-`
+`;
 const HomeButton = () => {
   return (
     <div>
-      <HomeButtonStyling>
-        <Link className="home-links" id="jumble-link" to="/jumble" className="links">
-          Jumble
-        </Link>
-      </HomeButtonStyling>
-        <HomeButtonStyling>
-        <Link className="home-links" id="spelling-link" to="/spelling" className="links">
-            Spelling
-        </Link>
-          </HomeButtonStyling>
+      <Link
+        className="home-links"
+        to="/jumble"
+        className="links"
+      >
+        <HomeButtonStyling>Jumble</HomeButtonStyling>
+      </Link>
+      <Link
+        className="home-links"
+        to="/spelling"
+        className="links"
+      >
+        <HomeButtonStyling>Spelling</HomeButtonStyling>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default HomeButton;
--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -4,80 +4,84 @@ import HomeButton from './HomeButton'
 
 const HomeStyling = styled.body`
   width: 100%;
-  height:100vh;
+  height: 100vh;
   display: block;
-  
-`
+  background: url("https://www.schemecolor.com/wallpaper?i=28824&desktop")
+    no-repeat center;
+  background-size: 100%;
+`;
 
 const TopBarStyling = styled.section`
   height: 60vh;
-  color: black;
+  color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-  background:url("https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-double-twelve-purple-gradient-e-commerce-promotion-geometry-banner-image_183414.jpg") no-repeat center;
-  background-size:cover;
-  
-  
-  h1{
+  /* background: url("https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-double-twelve-purple-gradient-e-commerce-promotion-geometry-banner-image_183414.jpg")
+    no-repeat center; */
+
+  header {
+    width: 70vw;
+    border: solid white 5px;
+    border-radius: 300px;
+    padding: 10px;
+    background: rgba(0, 0, 0, 0.5);
+    text-align: center;
+  }
+
+  h1 {
     font-weight: 700;
-    font-size: 6rem;
+    font-size: 7rem;
     color: white;
     margin: 0;
     letter-spacing: 3px;
-    border: solid white 5px;
-    border-radius: 300px;
-    padding: 50px;
-    background: rgba(0,0,0,.3);
-    margin-top: 50px;
+
+    /* background: url("https://www.schemecolor.com/wallpaper?i=28824&desktop") no-repeat center;
+    background-size: 190%; */
   }
 
-    h2{
-    color: #6392FF;
+  h2 {
+    color: #171717;
     color: white;
     font-weight: 400;
     font-size: 3rem;
     margin: 0;
   }
-
-`
+`;
 const BottomBarStyling = styled.section`
   height: 40vh;
-  color: #FF0067;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
   letter-spacing: 2px;
-  background: #171717;
+  background: rgba(0, 0, 0, 0.9);
 
-  p{
-    color: #777777;
-    font-size: 1.5rem;
+  p {
+    color: aquamarine;
+    font-size: 2rem;
     /* margin-bottom: 50px; */
   }
-  
-`
+`;
 
 class Homepage extends Component {
   render(){
-    return(
+    return (
       <HomeStyling>
         <TopBarStyling>
-          <h1>Letter-Jumble</h1>
-          <h2>for parents with little kids</h2>
-          
+          <header>
+            <h1>Letter-Jumble</h1>
+            <h2>for parents with little kids</h2>
+          </header>
         </TopBarStyling>
 
         <BottomBarStyling>
-          
           <p>Choose a game to play with your child</p>
-          <HomeButton/>
+          <HomeButton />
         </BottomBarStyling>
-        
       </HomeStyling>
-    )
+    );
   }
 }
 
